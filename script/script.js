ca999034082c081e8b2258eb9700dc3cb4e690e8
protocolisimo <protocolisimo@gmail.com>
2020-10-29 19:51:05 +0200

    finish slider

---
+++ script/script.js
@@ -0,0 +1,19 @@
+sliderSettings = () => {
+    $('#slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        fade: true,
+        draggable: false
+    });
+};
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    sliderSettings();
+    console.log('hi')
+});
